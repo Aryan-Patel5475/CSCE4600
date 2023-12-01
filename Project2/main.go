4f0f2b1c4bf361989a01e4c86d3d50c6aecb9b71
Aryan-Patel5475 <118410521+Aryan-Patel5475@users.noreply.github.com>
2023-12-01 17:52:39 -0600

    adding pwd again

--- Project2/main.go
+++ Project2/main.go
@@ -77,6 +77,8 @@ func handleInput(w io.Writer, input string, exit chan<- struct{}) error {
 		return builtins.ChangeDirectory(args...)
 	case "env":
 		return builtins.EnvironmentVariables(w, args...)
+	case "pwd":
+		return builtins.PrintWorkingDirectory()
 	case "exit":
 		exit <- struct{}{}
 		return nil
